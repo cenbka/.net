9d6881d9c5dbbd640452d0da4927c3219ebf9f15
cenbka <67373115+cenbka@users.noreply.github.com>
2021-11-16 13:18:01 +0300

    Add files via upload

---
+++ Task3/Task3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.OutBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(485, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Создать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(636, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Сохранить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Input1
+            // 
+            this.Input1.Location = new System.Drawing.Point(12, 153);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(600, 23);
+            this.Input1.TabIndex = 5;
+            this.Input1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // OutBox
+            // 
+            this.OutBox.Location = new System.Drawing.Point(12, 200);
+            this.OutBox.Name = "OutBox";
+            this.OutBox.Size = new System.Drawing.Size(600, 23);
+            this.OutBox.TabIndex = 6;
+            this.OutBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\Арсений\\source\\repos\\Task3\\Task3\\Файлы";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.InitialDirectory = "C:\\Users\\Арсений\\source\\repos\\Task3\\Task3\\Файлы";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 75);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 34);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Удалить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(159, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(135, 34);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Переименовать";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.OutBox);
+            this.Controls.Add(this.Input1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.TextBox OutBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Task3/Task3/MyDirectory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class MyDirectory
+    {
+        public string road;
+        public string name;
+        public List<MyFile> listD;
+
+
+        public MyDirectory(string road, string name, List<MyFile> listD)
+        {
+            this.road = road;
+            this.name = name;
+            this.listD = listD;
+        }
+
+        public string Road
+        {
+            get { return road; }
+            set { road = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+
+        public List<MyFile> ListD
+        {
+            get { return listD; }
+            set { listD = value; }
+        }
+    }
+}
